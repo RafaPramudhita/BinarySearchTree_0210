0180529a7445c0a857efc56907f4998831780d2b
RafaPramudhita <161433424+RafaPramudhita@users.noreply.github.com>
2024-06-13 16:03:13 +0700

    membuat prosedur search

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -47,7 +47,20 @@ public:
         }
     }
 
+    void search(string element, Node*& parent, Node*& currentNode)
+    {
+        currentNode = ROOT;
+        parent = NULL;
+        while ((currentNode != NULL) && currentNode->info != element)
+        {
+            parent = currentNode;
+            if (element < currentNode->info)
+                currentNode = currentNode->leftchild;
+            else
+                currentNode = currentNode->rightchild; 
+        }
+    }
+    
     
 };
-
 
