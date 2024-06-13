102e3d53e0e871b517fc0a81f5c92fd77a82ae34
RafaPramudhita <161433424+RafaPramudhita@users.noreply.github.com>
2024-06-13 15:24:48 +0700

    membuat constructor pada class node

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -8,7 +8,12 @@ public:
     Node* leftchild; 
     Node* rightchild; 
 
-    
+    // Connstruktor for the node class
+    Node(string i, Node* l, Node* r) {
+        info = i;
+        leftchild = l;
+        rightchild = r;
+    }
 };
 
 
